4ec618238551b5df3a1cf1a9c0a95d05e22f29b0
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-04-30 19:13:07 -0500

    first

---
+++ android/app/src/main/kotlin/com/example/matriculasappg15/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.matriculasappg15
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:matriculasappg15/pages/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
+}
